dfd1fea56ff8e5281c73eedeef3183feb558c8fa
vitthal-korvan <korvanvitthal@gmail.com>
2024-01-22 10:32:25 +0530

    about.js is updated!

--- DOM Assignment 1, 2, 3/about/about.js
+++ DOM Assignment 1, 2, 3/about/about.js
@@ -1,9 +1,9 @@
 let accordian = document.querySelectorAll(".accordian h3");
+
 accordian.forEach((element) => {
   element.addEventListener("click", () => {
-    
     let para = element.nextElementSibling;
-    para.style.backgroundColor="grey";
+
     if (para.style.display === "block") {
       para.style.display = "none";
     } else {
